d5462a44f8ccf2dc0926b0832941f60ea7e4b017
Rhettburnham <Rhett.burnham@emory.edu>
2024-11-23 15:09:09 -0500

    Update CORS settings

--- src/components/MainImg.jsx
+++ src/components/MainImg.jsx
@@ -2,17 +2,9 @@ import React from 'react';
 
 const MainImg = () => {
   return (
-    <div className="relative w-full max-w-screen-lg mx-auto">
-      {/* Top Triangle (Faint Color) */}
-      <div
-        className="absolute top-0 left-0 w-full h-16 faint-color"
-        style={{
-          clipPath: 'polygon(50% 100%, 0 0, 100% 0)',
-        }}
-      ></div>
-      
+    <div className="relative w-full min-w-screen-lg ">
       {/* Square Image */}
-      <div className="relative w-full aspect-square">
+      <div className="relative w-full">
         <img
           src="/assets/images/main_img.jpg"
           alt="Main"
@@ -22,9 +14,9 @@ const MainImg = () => {
 
       {/* Bottom Triangle (White) */}
       <div
-        className="absolute bottom-0 left-0 w-full h-16 bg-white"
+        className="absolute bottom-0 left-0 w-full h-[15vh] md:h-[20vh] bg-white"
         style={{
-          clipPath: 'polygon(50% 0, 0 100%, 100% 100%)',
+          clipPath: 'polygon(50% 0%, 0 100%, 100% 100%)',
         }}
       ></div>
     </div>
